a26c797cfefb5c33dc36fce1aa581c1880af73cc
Munetaka Mizutani <mu.mizutani@gmail.com>
2016-04-10 16:18:30 +0900

    collaborative filtering gradient

--- cousera/machine_learning/machine-learning-ex8/ex8/cofiCostFunc.m
+++ cousera/machine_learning/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,15 +40,10 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-J = 1 / 2 * sum(sum(((X * Theta' .- Y) .* R) .^ 2))
+J = 1 / 2 * sum(sum(((X * Theta' .- Y) .* R) .^ 2));
 
-
-
-
-
-
-
-
+X_grad = (X * Theta' .- Y) .* R * Theta;
+Theta_grad = ((X * Theta' .- Y) .* R)' * X;
 
 
 
